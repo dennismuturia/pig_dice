74c7933ef05fad96a1cdad8cc681a0f55d595715
dennismuturia <dmuturia2922@gmail.com>
2017-06-19 08:03:06 +0300

    Commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,16 @@
  *Created by Dennis Muturia, Elvis Mogaka
  *Version 1.0.0.0
 */
- //var total = 0;
- var scores = [];
 function Players1(points){
     this.points = points;
 }
 Players1.prototype.computeScores = function(randomValue){
     while (randomValue > 0){
-       
+       $('#name1').prop('disabled', true);
     if (randomValue === 1){
         alert("You have burnt");
         $('#Button').prop('disabled', true);
+        $('#Button1').prop('disabled', false);
         //scores.push(this.points);
         break;
         
@@ -21,29 +20,91 @@ Players1.prototype.computeScores = function(randomValue){
         return this.points += randomValue;
         }
     }
-}
+};
 
-function Players2(random2){
-    this.random2 = random2;
+Players1.prototype.holdCards = function(){
+     $('#Button').prop('disabled', true);
+     $('#Button1').prop('disabled', false);
+     alert("You have hold your cards at "+ this.points);
+};
+Players1.prototype.winner = function(){
+   if(this.points >= 100){
+        alert("You are the winner");
+        $('#Button').prop('disabled', true);
+        $('#Button1').prop('disabled', true);
+        
+    } 
+};
+function Players2(points){
+    this.points = points;
 }
+Players2.prototype.computeScores = function(randomValue){
+    while(randomValue > 0){
+         $('#name2').prop('disabled', true);
+        if (randomValue ===1){
+            alert("You have Burnt");
+            $('#Button').prop('disabled', false);
+            $('#Button1').prop('disabled', true);
+            break;
+        }else{
+            return this.points += randomValue;
+        }
+    }
+};
+Players2.prototype.holdCards = function(){
+     $('#Button').prop('disabled', false);
+     $('#Button1').prop('disabled', true);
+     alert("You have hold your cards at "+ this.points);
+};
+Players2.prototype.winner = function(){
+   if(this.points >= 100){
+        alert("You are the winner");
+        $('#Button').prop('disabled', true);
+        $('#Button1').prop('disabled', true);
+        
+    } 
+};
 
 //User Interface
 $(document).ready(function(){
+    //An object for player1
     var result  = new Players1(0);
+    //An object of player2
+    var result1  = new Players2(0);
     $('form#player1').submit(function(event){
         event.preventDefault();
-        var randomm = Math.floor(Math.random() * 6) + 1;      
+        var randomm = Math.floor(Math.random() * 6) + 1;
+        var name1 = $('input#name1').val();
         
+        
+        
+        $('#name11').text("Name: " + name1);
+        $('#numbers1').text(result.points);
         
         result.computeScores(randomm);
-        console.log(result.points);
+        result.winner();
+        
         });
     $('form#player2').submit(function(event){
         event.preventDefault();
         var random2 = Math.floor(Math.random() * 6) + 1;
+        var name2 = $('input#name2').val();
         
-        var result1  = new Players2(random2);
+         
+        $('#name12').text("Name: " + name2);
+        $('#numbers2').text(result1.points);
         
-        console.log(result1);
+        result1.computeScores(random2);
+         result1.winner();
+        });
+    $('form#hold').submit(function(event){
+        event.preventDefault();
+        
+        result.holdCards();
+        });
+    $('form#hold1').submit(function(event){
+        event.preventDefault();
+        
+        result1.holdCards();
         });
     });
